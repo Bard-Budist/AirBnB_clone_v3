5c2480b05c6c0b2a1d62973044c1dd7480e108a3
Bard-Budist <op7puro@live.com>
2020-04-28 15:53:32 +0000

    In app.py add rule app.url_map.strict_slashes = False for slashes in endpoint and in index add new route /stats to show all count of the class

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,16 +7,17 @@ import os
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(
         host=os.getenv('HBNB_API_HOST'),
         port=os.getenv('HBNB_API_PORT'),
         threaded=True)
-    
