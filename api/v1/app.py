277875278b1b0d7140d8a7992bab44d58d0d7cb5
Bard-Budist <op7puro@live.com>
2020-04-28 04:15:40 +0000

    Add some files about of api status - Add init files for management package - Add app.py Content the flask app and registers for blueprints - Add index in folder views The content is end_point /status for the API

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv('HBNB_API_HOST'),
+        port=os.getenv('HBNB_API_PORT'),
+        threaded=True)
+    
