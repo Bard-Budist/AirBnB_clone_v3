6cc5eb0ab6ca3a91bda0067936c2fbb8dc6a9ee6
Bard-Budist <op7puro@live.com>
2020-04-29 21:34:34 +0000

    enabling CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Main Flask app"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
@@ -8,6 +9,7 @@ import os
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, origins="0.0.0.0")
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
